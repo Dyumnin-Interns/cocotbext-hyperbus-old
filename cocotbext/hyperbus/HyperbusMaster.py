58df7fa60eb85e034a5478a52315066b30830205
Karthika Madhu <78961400+KM4500@users.noreply.github.com>
2023-12-19 00:13:42 +0530

    Update HyperbusMaster.py

--- cocotbext/hyperbus/HyperbusMaster.py
+++ cocotbext/hyperbus/HyperbusMaster.py
@@ -13,4 +13,17 @@ class HyperbusMaster(BusDriver):
         description of each parameter.
         '''
         pass
-    def _calc_burst()
+    def _calc_burst(data):
+        burst_length = data // 32
+        return burst_length if burst_length > 0 else 1
+
+class HyperbusAddressMap(BusDriver):
+    def _init_(self):
+        pass
+        '''
+        define mapping between logical and physical address     
+        '''
+
+class HyperbusTransactionGenerator(BusDriver):
+    def 
+
