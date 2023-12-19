24451b6b464cc8c2bd8f9787b4c6bfc81432aadd
Karthika Madhu <78961400+KM4500@users.noreply.github.com>
2023-12-19 12:44:56 +0530

    Update HyperbusSlave.py

--- cocotbext/hyperbus/HyperbusSlave.py
+++ cocotbext/hyperbus/HyperbusSlave.py
@@ -3,4 +3,10 @@ from cocotb_bus.drivers import BusDriver
 from cocotb.triggers import RisingEdge, FallingEdge, ReadOnly, Timer, ReadWrite, Lock
 
 class HyperbusSlave(BusDriver):
-  
+    def __init__(self,...):
+        pass
+    def write(self,address,data):
+      '''
+      '''
+    def read():
+      
