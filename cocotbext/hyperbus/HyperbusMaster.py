2b14c749684ac78c1e9b1dbb920ebcc43511c9bb
Karthika Madhu <78961400+KM4500@users.noreply.github.com>
2023-12-19 16:00:19 +0530

    added class templates

--- cocotbext/hyperbus/HyperbusMaster.py
+++ cocotbext/hyperbus/HyperbusMaster.py
@@ -1,6 +1,6 @@
 import cocotb
 from cocotb_bus.drivers import BusDriver
-from cocotb.triggers import RisingEdge, FallingEdge, ReadOnly, Timer, ReadWrite, clock
+from cocotb.triggers import RisingEdge, FallingEdge, ReadOnly, Timer, ReadWrite, Lock
 
 class HyperbusMaster(BusDriver):
     _signals=[...]
