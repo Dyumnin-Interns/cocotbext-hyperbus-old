2ba18de8f203ee9a597b1b8b9b5e5dbf032d616c
Karthika Madhu <78961400+KM4500@users.noreply.github.com>
2023-12-19 17:14:18 +0530

    added class templates

--- cocotbext/hyperbus/HyperbusSlave.py
+++ cocotbext/hyperbus/HyperbusSlave.py
@@ -2,4 +2,49 @@ import cocotb
 from cocotb_bus.drivers import BusDriver
 from cocotb.triggers import RisingEdge, FallingEdge, ReadOnly, Timer, ReadWrite, Lock
 
+
+
 class HyperbusSlave(BusDriver):
+    def __init__(self,...):
+        pass
+    def write(self, address, data):
+      '''
+      Writes data to the slave memory based on address, considering burst transfers.
+    Parameters:
+    address: The target address to write to.
+    data: The data to be written as a byte string.
+      '''
+        
+    def read(self, address, length):
+      '''
+       Reads data from the slave memory based on address
+      Parameters:
+      address: The target address to read from.
+      length: The number of bytes to read.
+      '''
+
+    def _calc_burst(data):
+        burst_length = data // 32
+        return burst_length if burst_length > 0 else 1
+
+
+
+    async def handle_transaction(self):
+         await RisingEdge(clk)
+        '''
+        chip select assertion check
+        '''
+
+    async def decode_command(self):
+        '''
+        using the ca bit assignment logic for(read/write/address space/burst type/latency)
+        '''
+
+   async def process_latency(self, latency):
+       '''
+       Implement logic to wait for required number of clock cycles based on latency
+       '''
+
+
+
+      
