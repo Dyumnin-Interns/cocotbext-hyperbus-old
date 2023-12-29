f79049f076b39e766e570e92b03bc463f423a461
Karthika Madhu <78961400+KM4500@users.noreply.github.com>
2023-12-29 13:14:46 +0530

    Added hdl

---
+++ hdl/hyperbusmaster.v
@@ -0,0 +1,16 @@
+module hyperbusmaster(
+    input wire clk,
+    input wire rst_n,
+    output reg cs,
+    inout wire dq,
+    output reg rwds,
+    input wire [31:0] address,
+    input wire [31:0] data_in,
+    output reg [31:0] data_out
+);
+
+
+
+
+
+endmodule
